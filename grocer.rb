86641557cd497a65627ab470a02bc17e0f1025c5
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-16 04:41:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -56,6 +56,6 @@ end
 
 def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
-  applied_coupons = apply_coupons(hash_cart)
+  applied_coupons = apply_coupons(hash_cart, coupons)
   
 end
